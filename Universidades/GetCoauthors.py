84525ede58b010eac77be60f947c250efa2c46ca
AlexVelezLl <alanvele@espol.edu.ec>
2020-05-28 09:39:02 -0500

    Presentacion

--- Universidades/GetCoauthors.py
+++ Universidades/GetCoauthors.py
@@ -21,6 +21,13 @@ def indexCS(data):
             indices.append(i)
     return indices
 
+def public2015_2019(data):
+    cont = 0
+    for p in data["Year"]:
+        if p>=2015 and p<=2019:
+            cont+=1
+    return cont
+
 def formating(author):
     author = author.strip()
     if not (author.endswith(".")):
@@ -42,14 +49,22 @@ with open(archivo_nodes,encoding='utf-8') as f:
         line2 = line.split(";")
         if len(line2)==5:
             print(line)"""
-
+publicaciones = 0
+pub2015_2019 = 0
+n_auth2015_2019=0
+totAuth = 0
+totArist= 0
+totNodesCG=0
+totEdgesCG=0
 for univ, archivo in zip(universidades,archivos):
     data = pd.read_excel(archivo)
     indices = indexCS(data)
+    publicaciones += len(indices)
     data = data.loc[indices]
     data.reset_index(inplace=True)
+    pub2015_2019+=public2015_2019(data)
     coauthors = []
-
+    auth2015_2019 = set()
     for author in data['Authors']:
         author = str(author)
         if ";" in author:
@@ -58,11 +73,13 @@ for univ, archivo in zip(universidades,archivos):
             coauthors.append(author.split(".,"))
 
     dic = {}
+    dicTot = {}
     authors = {}
     for i, publ in enumerate(coauthors):
 
         keywords = str(data['Keywords'][i]).split(",")
         year = data['Year'][i]
+
         for offset, author1 in enumerate(publ):
             author1 = formating(author1)
             if author1 not in authors.keys():
@@ -84,8 +101,21 @@ for univ, archivo in zip(universidades,archivos):
 
                 key = author1 + ";" + author2
                 altkey = author2 + ";" + author1
+                if key in dicTot.keys():
+                    dicTot[key]['n'] += 1
+
+                    if year < dicTot[key]['firstColab']:
+                        dicTot[key]['firstColab'] = year
+                elif altkey in dicTot.keys():
+                    dicTot[altkey]['n'] += 1
+                    if year < dicTot[altkey]['firstColab']:
+                        dicTot[altkey]['firstColab'] = year
+                else:
+                    dicTot[key] = {"n": 1, "firstColab": year}
 
                 if year >= 2015 and year <= 2019:
+                    auth2015_2019.add(author1)
+                    auth2015_2019.add(author2)
                     if key in dic.keys():
                         dic[key]['n'] += 1
 
@@ -115,7 +145,8 @@ for univ, archivo in zip(universidades,archivos):
     G = nx.from_pandas_edgelist(coau_df, "Source", "Target", ['Weight', 'Year'], create_using=nx.Graph())
     G = sorted(connected_component_subgraphs(G), key=len, reverse=True)[0]
     nodosCG = list(G.nodes)
-
+    totNodesCG+=len(nodosCG)
+    totEdgesCG+=len(dic.keys())
     with open("data/componente_gigante/coauthors-edgesCG-"+univ+".csv", "w", encoding="utf-8") as f:
         f.write("Source;Target;Weight;Year\n")
         for key, value in dic.items():
@@ -128,4 +159,9 @@ for univ, archivo in zip(universidades,archivos):
         for auth, keywords in authors.items():
             if auth in nodosCG:
                 g.write(auth + ";" + auth + ";" + (",".join(keywords['Before'])).replace(";","") +";"+(",".join(keywords['After'])).replace(";","")+ "\n")
+    totAuth += len(authors.keys())
+    totArist += len(dicTot.keys())
+    n_auth2015_2019 += len(auth2015_2019)
 
+print(totNodesCG)
+print(totEdgesCG)
